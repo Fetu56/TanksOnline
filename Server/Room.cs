b0b52fafbcfee73657f3e595cee622f333ff27fb
Al <Al@DESKTOP-JBMOFK9>
2021-11-02 23:33:38 +0200

    Added rooms

---
+++ Server/Room.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using TanksLib;
+using Timer = System.Timers.Timer;
+
+namespace Server
+{
+    class Room
+    {
+        public List<Client> clients;
+        public Room()
+        {
+            clients = new List<Client>();
+            Timer timer = new Timer();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 1000;
+            timer.Start();
+            Task refresher = new Task(Refresh);
+            refresher.Start();
+        }
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            RefreshActions();
+        }
+        public void Refresh()
+        {
+            while (!Environment.HasShutdownStarted)
+            {
+                try
+                {
+                    if (clients.Where(x => x.needToRef).Count() > 0)
+                    {
+                        Thread.Sleep(10);
+                        RefreshActions();
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex); }
+            }
+
+        }
+        private void SendMsgToAll(string msg)
+        {
+            for (int i = 0; i < clients.Count; i++)
+            {
+                try
+                {
+                    clients[i].Send(msg);
+                }
+                catch { }
+            }
+        }
+        public void RefreshActions()
+        {
+            List<Tank> tanks = new List<Tank>();
+            clients.ForEach(x => tanks.Add(x.socket.Connected && x.tankClient.hp > 0 ? x.tankClient : null));
+            SendMsgToAll(JsonSerializer.Serialize<List<Tank>>(tanks));
+            //Console.WriteLine($"Data sended to users, {tanks.Where(x => x != null).Count()} of tanks.");
+            clients.ForEach(x => x.needToRef = false);
+        }
+        public void DisconnectAll()
+        {
+            SendMsgToAll("→disconnect@");
+            clients.ForEach(x => x.Disconnect());
+            clients.Clear();
+        }
+        public override string ToString()
+        {
+            return $"Room was visited with {clients.Count} players.";
+        }
+    }
+}
--- Server/Server.cs
+++ Server/Server.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 using TanksLib;
-using Timer = System.Timers.Timer;
+
 
 namespace Server
 {
@@ -17,24 +17,18 @@ namespace Server
     {
         Socket socket;
         IPEndPoint iPEndPoint;
-        List<Client> clients;
+        List<Room> rooms;
         Task senderMsg;
         bool work = true;
         public Server()
         {
             iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            clients = new List<Client>();
-            Timer timer = new Timer();
-            timer.Elapsed += Timer_Elapsed;
-            timer.Interval = 1000;
-            timer.Start();
+            rooms = new List<Room>();
+           
         }
 
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            RefreshActions();
-        }
+        
 
         public Server(string ip, int port)
         {
@@ -58,8 +52,7 @@ namespace Server
                 socket.Listen(10);
                 senderMsg = new Task(msgSender);
                 senderMsg.Start();
-                Task refresher = new Task(Refresh);
-                refresher.Start();
+                
                 while (work)
                 {
                     Connected();
@@ -70,51 +63,38 @@ namespace Server
                 Console.WriteLine(ex.Message);
             }
 
-            DisconnectAll();
+            rooms.ForEach(x => x.DisconnectAll());
 
         }
         private void Connected()
         {
             Socket socketClient = socket.Accept();
-            if(clients.Where(x => x.tankClient.hp > 0 && x.socket.Connected).Count() < 4)
+            bool joined = false;
+            for(int i = 0; i < rooms.Count; i++)
             {
-                clients.Add(new Client(socketClient));
-                Console.WriteLine($"Registrated user join. Info: ip - {socketClient.RemoteEndPoint}, protocol {socketClient.ProtocolType}");
-                socketClient.Send(Encoding.Unicode.GetBytes(clients.Count.ToString() + ','));
-            }
-            
-
-        }
-
-        public void Refresh()
-        {
-            while(!Environment.HasShutdownStarted)
-            {
-                try
+                if (rooms[i].clients.Where(x => x.tankClient.hp > 0 && x.socket.Connected).Count() < 4)
                 {
-                    if (clients.Where(x => x.needToRef).Count() > 0)
-                    {
-                        Thread.Sleep(10);
-                        RefreshActions();
-                    }
+                    rooms[i].clients.Add(new Client(socketClient));
+                    joined = true;
+                    Console.WriteLine($"Registrated user join room #{i}. Info: ip - {socketClient.RemoteEndPoint}, protocol {socketClient.ProtocolType}");
+                    socketClient.Send(Encoding.Unicode.GetBytes(rooms[i].clients.Count.ToString() + ','));
+                    return;
                 }
-                catch(Exception ex) { Console.WriteLine(ex); }
+            }
+            if(!joined)
+            {
+                Room room = new Room();
+                room.clients.Add(new Client(socketClient));
+                Console.WriteLine($"Registrated user join NEW room #{rooms.Count}. Info: ip - {socketClient.RemoteEndPoint}, protocol {socketClient.ProtocolType}");
+                socketClient.Send(Encoding.Unicode.GetBytes(room.clients.Count.ToString() + ','));
+                rooms.Add(room);
             }
             
+
         }
-        public void RefreshActions()
-        {
-            List<Tank> tanks = new List<Tank>();
-            clients.ForEach(x => tanks.Add(x.socket.Connected && x.tankClient.hp > 0 ? x.tankClient : null));
-            SendMsgToAll(JsonSerializer.Serialize<List<Tank>>(tanks));
-            Console.WriteLine($"Data sended to users, {tanks.Where(x => x != null).Count()} of tanks.");
-            clients.ForEach(x => x.needToRef = false);
-        }
-        public void DisconnectAll()
-        {
-            clients.ForEach(x => x.Disconnect());
-            clients.Clear();
-        }
+
+        
+        
         private void msgSender()
         {
             string msg;
@@ -122,16 +102,13 @@ namespace Server
             {
                 try
                 {
-                    Console.WriteLine("Выберите действие:\n[1] - отключение\n[2] - обновить\n[0] - выход:");
+                    Console.WriteLine("Выберите действие:\n[1] - отключение\n[0] - выход:");
                     msg = Console.ReadLine();
                     Option option = Option.nullOp;
                     switch (msg)
                     {
                         case "1":
                             option = Option.disconnect;
-                            break;
-                        case "2":
-                            option = Option.startProg;
                             break;
                         case "0":
                             option = Option.exit;
@@ -148,12 +125,12 @@ namespace Server
                     }
                     else if (option != Option.nullOp)
                     {
-                        if (clients.Count > 0)
+                        if (rooms.Count > 0)
                         {
-                            Console.WriteLine("Выберите клиента, к которому применить действие. Для выбора всех введите \"!\".");
-                            for (int i = 0; i < clients.Count; i++)
+                            Console.WriteLine("Выберите комнату, к которой применить действие. Для выбора всех введите \"!\".");
+                            for (int i = 0; i < rooms.Count; i++)
                             {
-                                Console.WriteLine(i + ". " + clients[i]);
+                                Console.WriteLine(i + ". " + rooms[i]);
                             }
                             string get = Console.ReadLine();
                             switch (option)
@@ -161,18 +138,13 @@ namespace Server
                                 case Option.disconnect:
                                     if (get.StartsWith("!"))
                                     {
-                                        SendMsgToAll("→disconnect@");
-                                        DisconnectAll();
+                                        rooms.ForEach(x => x.DisconnectAll());
                                     }
                                     else
                                     {
                                         int id = int.Parse(get);
-                                        clients[id].Send("→disconnect@");
-                                        clients[id].Disconnect();
+                                        rooms[id].DisconnectAll();
                                     }
-                                    break;
-                                case Option.startProg:
-                                    RefreshActions();
                                     break;
                             }
                         }
@@ -188,17 +160,7 @@ namespace Server
                 }
             }
         }
-        private void SendMsgToAll(string msg)
-        {
-            for(int i = 0; i < clients.Count; i++)
-            {
-                try
-                {
-                    clients[i].Send(msg);
-                }
-                catch { }
-            }
-        }
+        
         private string GetString(Socket sc)
         {
             StringBuilder stringBuilder = new StringBuilder();
